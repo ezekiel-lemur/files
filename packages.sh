5caa9b39252189a1c531f3f7f108726dab71c7a2
Ezekiel <ezekields@posteo.eu>
2018-09-09 09:47:57 +0100

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pacaur -S alsa-utils arandr arc-gtk-theme arc-icon-theme-git bluez bluez-utils
-pacaur -S chromium compton dhcpcd discord dropbox feh file-roller firefox git glibc grep
+pacaur -S chromium compton dhcpcd discord dropbox feh file-roller firefox git glibc grep gnome
 pacaur -S gvfs gzip i3-gaps i3blocks i3status lastpass-cli lxappearance-gtk3-git mlocate 
 pacaur -S netctl networkmanager pacaur pavucontrol polybar pulseaudio pulseaudio-alsa 
 pacaur -S pulseaudio-bluetooth ranger rofi spotify stow sublime-text-dev termite thunar
